745bc5bf596de0e98feed580d0412e495c6d8b18
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-29 17:03:26 +0000

    Added main program.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit3-01-HTML
+// March 29 2022
 
 'use strict'
 /**
- * Alerts "Hello, World!"
+ * Calculates the area of a trapezoid using the user's inputs.
  */
-function onButtonClick() {
-  alert("Hello, World!")
+function calculate() {
+  const sidea = parseInt(document.getElementById("sidea").value)
+  const sideb = parseInt(document.getElementById("sideb").value)
+  const height = parseInt(document.getElementById("height").value)
+
+  document.getElementById("output").innerHTML = "<p>The trapezoid's area is " + (((sidea + sideb) / 2) * height) + "cm²</p>"
 }
